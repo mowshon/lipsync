b97b58042bc1d433da1b669ba969f56181cf2135
mowshon <vasile.buldumac@ati.utm.md>
2024-12-12 20:33:34 +0200

    A small improvement for: Conv2d and Conv2dTranspose

--- lipsync/models/conv.py
+++ lipsync/models/conv.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 
@@ -11,74 +12,27 @@ class Conv2d(nn.Module):
         kernel_size (int or tuple): Size of the convolving kernel.
         stride (int or tuple): Stride of the convolution.
         padding (int or tuple): Zero-padding added to both sides of the input.
-        residual (bool): If True, adds the input to the output (residual connection). (default is False)
+        residual (bool): If True, adds the input to the output (residual connection). Default is False.
 
     Attributes:
-        conv_block (nn.Sequential): Sequential container with convolution and batch normalization layers.
+        conv_block (nn.Sequential): Convolution and batch normalization layers.
         act (nn.ReLU): ReLU activation function.
         residual (bool): Whether to apply a residual connection.
     """
 
-    def __init__(self, cin, cout, kernel_size, stride, padding, residual=False, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, cin, cout, kernel_size, stride, padding, residual=False):
+        super().__init__()
         self.conv_block = nn.Sequential(
             nn.Conv2d(cin, cout, kernel_size, stride, padding),
             nn.BatchNorm2d(cout)
         )
-        self.act = nn.ReLU()
+        self.act = nn.ReLU(inplace=True)
         self.residual = residual
 
-    def forward(self, x):
-        """
-        Forward pass through the Conv2d layer.
-
-        Args:
-            x (torch.Tensor): Input tensor.
-
-        Returns:
-            torch.Tensor: Output tensor after applying the convolution, batch normalization, and activation.
-        """
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.conv_block(x)
         if self.residual:
-            out += x
-        return self.act(out)
-
-
-class nonorm_Conv2d(nn.Module):
-    """
-    Convolutional layer without batch normalization, using LeakyReLU as activation.
-
-    Args:
-        cin (int): Number of input channels.
-        cout (int): Number of output channels.
-        kernel_size (int or tuple): Size of the convolving kernel.
-        stride (int or tuple): Stride of the convolution.
-        padding (int or tuple): Zero-padding added to both sides of the input.
-        residual (bool): If True, adds the input to the output (residual connection). (default is False)
-
-    Attributes:
-        conv_block (nn.Sequential): Sequential container with convolution.
-        act (nn.LeakyReLU): LeakyReLU activation function.
-    """
-
-    def __init__(self, cin, cout, kernel_size, stride, padding, residual=False, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.conv_block = nn.Sequential(
-            nn.Conv2d(cin, cout, kernel_size, stride, padding),
-        )
-        self.act = nn.LeakyReLU(0.01, inplace=True)
-
-    def forward(self, x):
-        """
-        Forward pass through the nonorm_Conv2d layer.
-
-        Args:
-            x (torch.Tensor): Input tensor.
-
-        Returns:
-            torch.Tensor: Output tensor after applying the convolution and LeakyReLU activation.
-        """
-        out = self.conv_block(x)
+            out = out + x
         return self.act(out)
 
 
@@ -92,30 +46,21 @@ class Conv2dTranspose(nn.Module):
         kernel_size (int or tuple): Size of the transposed convolutional kernel.
         stride (int or tuple): Stride of the transposed convolution.
         padding (int or tuple): Zero-padding added to both sides of the input.
-        output_padding (int or tuple): Additional size added to the output shape. (default is 0)
+        output_padding (int or tuple): Additional size added to the output shape. Default is 0.
 
     Attributes:
-        conv_block (nn.Sequential): Sequential container with transposed convolution and batch normalization layers.
+        conv_block (nn.Sequential): Transposed convolution and batch normalization layers.
         act (nn.ReLU): ReLU activation function.
     """
 
-    def __init__(self, cin, cout, kernel_size, stride, padding, output_padding=0, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, cin, cout, kernel_size, stride, padding, output_padding=0):
+        super().__init__()
         self.conv_block = nn.Sequential(
             nn.ConvTranspose2d(cin, cout, kernel_size, stride, padding, output_padding),
             nn.BatchNorm2d(cout)
         )
-        self.act = nn.ReLU()
+        self.act = nn.ReLU(inplace=True)
 
-    def forward(self, x):
-        """
-        Forward pass through the Conv2dTranspose layer.
-
-        Args:
-            x (torch.Tensor): Input tensor.
-
-        Returns:
-            torch.Tensor: Output tensor after applying the transposed convolution, batch normalization, and activation.
-        """
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.conv_block(x)
         return self.act(out)
